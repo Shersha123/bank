8a420260549cbca2128076a3d6c99b99c6dee451
shersha123 <ukkasnaina99@gmail.com>
2023-04-03 09:44:03 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-import { AddForm } from "./AddForm";
-import { UserList } from './UserList';
-import { Employee } from './Employee';
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./App.css";
+import userList from "./data.js";
+import UserTable from "./tables/UserTable";
+import AddUserForm from "./forms/AddUserForm";
+import EditUserForm from "./forms/EditUserForm";
 
+const App = () => {
+  const [users, setUsers] = useState(userList);
 
-function App() {
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const [editing, setEditing] = useState(false);
+
+  const initialUser = {
+    id: null,
+    name: "",
+    lastName: "",
+    mobileNumber: "",
+    email: "",
+    dateOfBirth: "",
+    address: "",
+    accountNumber: "",
+    ifscCode: "",
+    bankName: "",
+    branch: "",
+  };
+
+  const [currentUser, setCurrentUser] = useState(initialUser);
+
+  const editUser = (id, user) => {
+    setEditing(true);
+    setCurrentUser(user);
+  };
+
+  const updateUser = (newUser) => {
+    setUsers(
+      users.map((user) => (user.id === currentUser.id ? newUser : user))
+    );
+    setCurrentUser(initialUser);
+    setEditing(false);
+  };
 
   return (
+    <>
+      {/* <h1>React CRUD App with Hooks</h1> */}
+      <div className="container">
+        {editing ? (
+          <EditUserForm
+            currentUser={currentUser}
+            setEditing={setEditing}
+            updateUser={updateUser}
+          />
+        ) : (
+          <AddUserForm addUser={addUser} />
+        )}
 
-    <div>
-      <Routes>
-        <Route path="/" element={<Employee />} />
-      </Routes>
-    </div>
-
+        <div className="userlist-container">
+          <h2>View users</h2>
+          <UserTable
+            users={users}
+            deleteUser={deleteUser}
+            editUser={editUser}
+          />
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
